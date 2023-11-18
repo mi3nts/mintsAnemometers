f09497c6b2239d3877604f3641bf67cdbe966a9a
lakihaomal <lhw150030@utdallas.edu>
2023-11-18 08:13:18 -0600

    rsn012 updates

--- firmware/xu4Mqtt/runAll.sh
+++ firmware/xu4Mqtt/runAll.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-#
+
 sleep 60
 
 kill $(pgrep -f 'python3 airMarReader.py')
-sleep 5
+sleep 1
 python3 airMarReader.py &
-
 sleep 5
+
+kill $(pgrep -f 'python3 rsn012Reader.py')
+sleep 1
+python3 rsn012Reader.py &
+sleep 5
+
 python3 ipReader.py
+
