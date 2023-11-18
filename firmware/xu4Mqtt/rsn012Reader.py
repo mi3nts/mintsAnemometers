35b37e0bf635d245f8dcb0e344c8016fdf76be56
lakihaomal <lhw150030@utdallas.edu>
2023-11-18 07:58:23 -0600

    rsn012 updates

--- firmware/xu4Mqtt/rsn012Reader.py
+++ firmware/xu4Mqtt/rsn012Reader.py
@@ -15,7 +15,7 @@ dataFolder    =  mD.dataFolder
 rsn012Port    =  str(mD.rsn012Port[0])
 baudRate      = 4800
 responseTime  = 1
-deltaTime     = .000994
+deltaTime     = .001
 sensorName    = "RSN012"
 
 windSpeedDirectionRequest = bytearray([
