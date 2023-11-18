e284a29b2213aabe46cf2dcd37dccecc3a642728
lakihaomal <lhw150030@utdallas.edu>
2023-11-18 07:23:14 -0600

    rsn012 updates

--- firmware/xu4Mqtt/rsn012Reader.py
+++ firmware/xu4Mqtt/rsn012Reader.py
@@ -8,87 +8,67 @@ import serial
 from collections import OrderedDict
 
 dataFolder    =  mD.dataFolder
-airmarPort    =  mD.airmarPort[0]
-print(airmarPort)
+rsn012Port    =  mD.rsn012Port[0]
+print(rsn012Port)
+
+baudRate = 4800
+
+windSpeedDirectionRequest = bytearray([
+                                0x01,  # Device address
+                                0x03,  # Function code for reading holding registers
+                                0x00, 0x00, # Register start address
+                                0x00, 0x02, # Data Length
+                                0xC4, 0x0B  # Checksum
+                            ])
+
+# windLevelRequest = bytearray([
+#                                 0x01,  # Device address
+#                                 0x03,  # Function code for reading holding registers
+#                                 0x00, 0x04, # Register start address
+#                                 0x00, 0x01, # Data Length
+#                                 0xC5, 0xCB  # Checksum
+#                             ])
 
 def main():
-
     ser = serial.Serial(
-    port= airmarPort,\
-    baudrate=4800,\
-    parity  =serial.PARITY_NONE,\
-    stopbits=serial.STOPBITS_ONE,\
-    bytesize=serial.EIGHTBITS,\
-    timeout=0)
+            port = rsn012Port,
+            baudrate= baudRate,
+            timeout=0
+            parity  =serial.PARITY_NONE,\
+            stopbits=serial.STOPBITS_ONE,\
+            bytesize=serial.EIGHTBITS,\
+            timeout=0)
+    
 
-    lastHCHDT = time.time()
-    lastWIMWV = time.time()
-    lastGPGGA = time.time()
-    lastGPVTG = time.time()
-    lastGPZDA = time.time()
-    lastWIMDA = time.time()
-    lastYXXDR = time.time()
 
-    delta  = .5
-    print("connected to: " + ser.portstr)
-
-    #this will store the line
-    line = []
     while True:
         try:
-            for c in ser.read():
-                line.append(chr(c))
-                if chr(c) == '\n':
-                    dataString     = (''.join(line)).replace("\r\n","")
-                    dateTime  = datetime.datetime.now()
-                    print(dataString)
+            print("---------===============----------")
+            dateTime  = datetime.datetime.now()
+            ser.write(windSpeedDirectionRequest)
+            response = ser.read(9)
+            print(dateTime)
+            print(len(response))
+            for x in response:
+                print(str(x)+ " - Hex:" +  hex(x)) 
+            speed          = (response[3] << 8) | response[4]
+            windDirection  = (response[5] << 8) | response[6]
+            print("Speed = " + str(speed/100) + " m/s")
+            print("Wind Direction = " + str(windDirection) + " degrees north")            
+            time.sleep(1)
 
-                    if (dataString.startswith("$HCHDT") and mSR.getDeltaTimeAM(lastHCHDT,delta)):
-                        mSR.HCHDTWriteAM(dataString,dateTime)
-                        lastHCHDT = time.time()
-                    # print(str(dataString))
+        except Exception as e:
+            # Handle any type of exception
+            print(f"An error occurred: {e}")
 
-                    if (dataString.startswith("$WIMWV") and mSR.getDeltaTimeAM(lastWIMWV,delta)):
-                        mSR.WIMWVWriteAM(dataString,dateTime)
-                        lastWIMWV = time.time()
-                    # print(str(dataString))
-
-                    if (dataString.startswith("$GPGGA") and mSR.getDeltaTimeAM(lastGPGGA,delta)):
-                        mSR.GPGGAWriteAM(dataString,dateTime)
-                        lastGPGGA = time.time()
-                    # print(str(dataString))
-
-                    if (dataString.startswith("$GPVTG") and mSR.getDeltaTimeAM(lastGPVTG,delta)):
-                        mSR.GPVTGWriteAM(dataString,dateTime)
-                        lastGPVTG = time.time()
-                    # print(str(dataString))
-
-                    if (dataString.startswith("$GPZDA") and mSR.getDeltaTimeAM(lastGPZDA,delta)):
-                        mSR.GPZDAWriteAM(dataString,dateTime)
-                        lastGPZDA = time.time()
-                    # print(str(dataString))
-
-                    if (dataString.startswith("$WIMDA") and mSR.getDeltaTimeAM(lastWIMDA,delta)):
-                        mSR.WIMDAWriteAM(dataString,dateTime)
-                        lastWIMDA = time.time()
-                    # print(str(dataString))
-            
-                    
-                    if (dataString.startswith("$YXXDR,A") and mSR.getDeltaTimeAM(lastYXXDR,delta)):
-                        mSR.YXXDRWriteAM2(dataString,dateTime)
-                        lastYXXDR = time.time()
-                    # print(str(dataString))
-
-                    line = []
-                    break
-        except:
-            print("Incomplete String Read")
-            line = []
-                    
     ser.close()
 
 
 
 if __name__ == "__main__":
-   main()
+    print("=============")
+    print("    MINTS    ")
+    print("=============")
+    print("Monitoring RSN012 Sensor on port: {0}".format(rsn012Port[0])+ " with baudrate " + str(baudRate))
+    main()
 
