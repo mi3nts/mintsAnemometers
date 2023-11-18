e15bcf359c2be74a4a52652eaca07d88f142f1d5
lakithaomal <lhw150030@utdallas.edu>
2023-11-18 06:12:45 +0000

    airmar updates

--- firmware/xu4Mqtt/killAll.sh
+++ firmware/xu4Mqtt/killAll.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 #
-sleep 20
-kill $(pgrep -f 'ips7100ReaderV1.py')
-sleep 1
-kill $(pgrep -f 'python3 rainReader.py')
-sleep 1
-kill $(pgrep -f 'python3 i2cReader.py')
 sleep 1
 kill $(pgrep -f 'python3 airMarReader.py')
--- firmware/xu4Mqtt/runAll.sh
+++ firmware/xu4Mqtt/runAll.sh
@@ -2,20 +2,9 @@
 #
 sleep 60
 
-kill $(pgrep -f 'ips7100ReaderV1.py')
-sleep 5
-python3 ips7100ReaderV1.py &
-sleep 5
-kill $(pgrep -f 'python3 rainReader.py')
-sleep 5
-python3 rainReader.py &
-sleep 5
-kill $(pgrep -f 'python3 i2cReader.py')
-sleep 5
-python3 i2cReader.py &
 kill $(pgrep -f 'python3 airMarReader.py')
 sleep 5
 python3 airMarReader.py &
+
 sleep 5
 python3 ipReader.py
-sleep 5
